373971e3b0b04a1109836359d0a97fe9d2d1634c
sankalpsriva <ssankalp312@gmail.com>
2022-04-27 21:54:33 -0400

    update, with camera movement

--- ursinaTest.py
+++ ursinaTest.py
@@ -1,3 +1,26 @@
 from ursina import *
-
+from random import randint
+def update():
+    global x, speed
+    x = x + time.dt * speed
+    # cube.x = cube.x + time.dt * .5
+    # red  = randint(0,255)
+    # # blue = randint(0,255)
+    # # green = randint(0,255)
+    # # cube.color = color.rgb(red, green, blue)
+    # cube.rotation.x = cube.rotation.x + time.dt * 100
+    # cube.rotation.y = cube.rotation.y + time.dt * 100
+    if abs(x) > 3:
+        speed = speed * -1
+    camera.position = (x, 0, -20)
 app = Ursina()
+
+# Model = "cube", "circle", "quad", "sphere"                                        color.rgb(r, g ,b)
+cube = Entity(model = "cube", color = color.rgb(255, 0, 0), scale = (1, 2, 5))
+
+x = 0
+speed = 1
+txt = Text(text = "this is a red cube", x = .3, y = .3)
+camera.position = (0, 0, 0)
+
+app.run()
